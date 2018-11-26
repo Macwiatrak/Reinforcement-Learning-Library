22921cee278d1abe762fbc6f1e7c7b6a6e2a66c5
Maciek Wiatrak <maciej.wiatrak.16@ucl.ac.uk>
2018-11-26 23:26:08 +0000

    added contextual-policy.ipynb

---
+++ spinningup/deterministic-policy.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+
+obs = tf.placeholder(shape=(None, obs_dim), dtype=tf.float32)
+net = mlp(obs, hidden_dims=(64,64), activation=tf.tanh)
+actions = tf.layers.dense(net, units=act_dim, activation=None)
